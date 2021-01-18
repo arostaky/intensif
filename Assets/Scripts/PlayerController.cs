a3f899e2b2de2e9f54a476893f71d396aa21661d
Patricio <jpdibacco@gmail.com>
2021-01-18 10:05:53 +0100

    adding test level and postprocessing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,47 +1,83 @@
+using System;
+using System.Collections;
 using UnityEngine;
- 
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController _controller;
-    public float _speed = 10;
+    public CharacterController controller;
+    public float speed = 10;
     public float _rotationSpeed = 90f;
  
     private Vector3 rotation;
     private Vector3 newRotation;
     private Vector3 currentPosition;
+
+    public GameObject CameraToRotate;
+    public float rotationSpeed = 2.5f;
+    private bool rotateL = false;
+    // private bool isRotating = false;
+    private bool rotateR =  false;
+    private bool groundedPlayer;
+    //private Vector3 playerVelocity;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.0f;
+    private Vector3 move;
+    private bool jump;
     void Start(){
-        newRotation = new Vector3(0,90f,0);
-        transform.Rotate(newRotation);
+        // newRotation = new Vector3(0,90f,0);
+        // transform.Rotate(newRotation);
     }
-    public void Update()
-    {
-        // this.rotation = new Vector3(0, _rotationSpeed * Time.deltaTime, 0);
- 
-        Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Horizontal") * Time.deltaTime);
+    void Update(){
+        //capture controls here:
+        move = new Vector3(0, 0, Input.GetAxisRaw("Horizontal") * Time.deltaTime);
+        jump =  Input.GetButtonDown("Jump");
+        
+        //Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Horizontal") * Time.deltaTime);
         move = transform.TransformDirection(move);
-        _controller.Move(move * _speed);
+        controller.Move(move * speed);
         currentPosition = transform.position;
+               
+        groundedPlayer = controller.isGrounded;
+        
+        if (groundedPlayer && move.y < 0){
+            move.y = 0f;
+        }
+         if ( jump && groundedPlayer){
+            move.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+        //Debug.Log(playerVelocity.y);
+        move.y += gravity * Time.deltaTime;
+        controller.Move(move * Time.deltaTime);
+        // if (rotateL == true){
+        //     StartCoroutine("rotateLeft");
+        // }
+        // if (rotateR == true){
+        //      StartCoroutine("rotateRight");
+        // }
         
     }
-    // public void RotatePlayer(float rotationValue){
-    //     Debug.Log("now i will rotate!");
-    //     _controller.Move(new Vector3(0f,0f,0f));
-    //      transform.Rotate(0f,rotationValue,0f);
-    //     // this.transform.Rotate(new Vector3(0f,rotationValue,0f));
-    //     //newRotation = new Vector3(0,rotationValue,0);
-    // }
+    void FixedUpdate(){
+        
+    }
     void OnTriggerEnter(Collider other) {
-        // _controller.enabled = false;
         switch(other.tag){
             case "RotateLeft": 
-                // transform.position = currentPosition;
-                transform.localRotation =  Quaternion.Euler(new Vector3(0, 0 * Time.deltaTime, 0));
-                // transform.Rotate(0,0,0); 
-                Debug.Log("I will rotate left");
-                // _controller.enabled = true; 
+                rotateL = true;
+                rotateR = false;
                 break;
-            case "RotateRight": transform.Rotate(0,-90,0); break;
-            //case "Finish": FinishGame(); break;
+            case "RotateRight":
+                rotateL = false;
+                rotateR = true;
+                break;
         }
+    }
+    public IEnumerator rotateLeft(){
+        transform.localRotation = Quaternion.Slerp(transform.localRotation,Quaternion.Euler(new Vector3(0,0, 0)), Time.deltaTime * rotationSpeed);
+        CameraToRotate.transform.localRotation = Quaternion.Slerp(CameraToRotate.transform.localRotation,Quaternion.Euler(new Vector3(0,-90, 0)), Time.deltaTime * rotationSpeed);
+        yield return null;
+    }
+     public IEnumerator rotateRight(){
+        transform.localRotation = Quaternion.Slerp(transform.localRotation,Quaternion.Euler(new Vector3(0,90, 0)), Time.deltaTime * rotationSpeed);
+        CameraToRotate.transform.localRotation = Quaternion.Slerp(CameraToRotate.transform.localRotation,Quaternion.Euler(new Vector3(0,0, 0)), Time.deltaTime * rotationSpeed);
+        yield return null;
     }
 }
---
+++ Assets/Scripts/SmoothRotation.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothRotation : MonoBehaviour{
+    [Range(0,360)]public float angle;//Specify Angle For Rotation
+    float tempAngle;//Temporary Angle Measurement Variable
+    bool horizontalFlag;//Check For Horizontal Roation
+    bool verticalFlag;//Check For Vertical Roation
+    bool isRotating;//Check Whether Currently Object is Rotating Or Not.
+    int Direction;//Direction Of Rotation
+ 
+    //Called For Initialization
+    void Start () {
+        horizontalFlag=verticalFlag=isRotating=false;
+    }
+ 
+    //Method For Horizontal Input
+    void CheckForHorizontalInput(){
+        if(Input.GetAxis("Horizontal")!=0 && !isRotating)
+        {
+            isRotating=true;
+            Direction=(Input.GetAxis("Horizontal")<0?-1:1);
+            horizontalFlag=true;
+            tempAngle=0;
+        }
+    }
+ 
+    //Method For Vertical Input
+    void CheckForVerticalInput(){
+        if(Input.GetAxis("Vertical")!=0 && !isRotating)
+        {
+            isRotating=true;
+            Direction=(Input.GetAxis("Vertical")<0?-1:1);
+            verticalFlag=true;
+            tempAngle=0;
+        }
+    }
+ 
+    //Method For horizontal Rotation
+    void HorizontalRotation(){
+        transform.Rotate(Vector3.up*angle*Time.fixedDeltaTime*Direction,Space.World);
+        tempAngle+=angle*Time.fixedDeltaTime;
+        if(tempAngle>=angle)
+        {
+            tempAngle=0;
+            isRotating=false;
+            horizontalFlag=false;
+        }
+    }
+ 
+    //Method For Vertical Rotation
+    void VerticalRotation()
+    {
+        transform.Rotate(Vector3.right*angle*Time.fixedDeltaTime*Direction,Space.World);
+        tempAngle+=angle*Time.fixedDeltaTime;
+        if(tempAngle>=angle)
+        {
+            tempAngle=0;
+            isRotating=false;
+            verticalFlag=false;
+        }
+    }
+    
+    void Update () {
+ 
+        CheckForHorizontalInput();
+        CheckForVerticalInput();
+        if(horizontalFlag)
+            HorizontalRotation();
+        if(verticalFlag)
+            VerticalRotation();
+ 
+    }
+}
