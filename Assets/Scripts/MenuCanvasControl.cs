849c2b53384c9b3f677853377a093889e48e2e04
Patricio <jpdibacco@gmail.com>
2021-01-22 17:13:20 +0100

    added menu script and fire attack

---
+++ Assets/Scripts/MenuCanvasControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCanvasControl : MonoBehaviour
+{
+    public float finishTime = 400.0f;
+    public CanvasGroup menu;
+    private float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime / finishTime;
+        menu.alpha = Mathf.Lerp(0,1, Time.time*t);
+    }
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bulletPrefab;
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+    void Shoot(){
+        Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+    }
+}
